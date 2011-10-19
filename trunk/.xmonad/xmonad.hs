aff51ac023ec2ab268c4a3d0c105264e626cbfda
ah.duros <ah.duros@04348482-9762-11de-926f-6fb3983382c0>
2011-10-19 23:54:21 +0000

    Backlogged stuff.

--- trunk/.xmonad/xmonad.hs
+++ trunk/.xmonad/xmonad.hs
@@ -1,6 +1,6 @@
 import XMonad
 import XMonad.Config.Desktop
-import XMonad.Config.Gnome
+import XMonad.Config.Xfce
 import XMonad.Util.EZConfig
 import qualified XMonad.StackSet as W
 import qualified Data.Map as M
@@ -21,50 +21,17 @@ import DBus.Message
 import Control.Monad
 import Control.OldException
 
--- This retry is really awkward, but sometimes DBus won't let us get our
--- name unless we retry a couple times.
-getWellKnownName :: Connection -> IO ()
-getWellKnownName dbus = tryGetName `catchDyn` (\ (DBus.Error _ _) ->
-                                                getWellKnownName dbus)
- where
-  tryGetName = do
-    namereq <- newMethodCall serviceDBus pathDBus interfaceDBus "RequestName"
-    addArgs namereq [String "org.xmonad.Log", Word32 5]
-    sendWithReplyAndBlock dbus namereq 0
-    return ()
-
 myLayout = named "Tall" layout ||| named "Wide" (Mirror layout) ||| noBorders Full
     where
         tall = Tall 1 (3/100) 0.6
         layout = smartBorders tall
 
 main = withConnection Session $ \ dbus -> do
-  putStrLn "Getting well-known name."
   getWellKnownName dbus
-  putStrLn "Got name, starting XMonad."
-  xmonad $ gnomeConfig {
-      logHook    = dynamicLogWithPP $ defaultPP {
-                   ppOutput   = \ str -> do
-                     let str'  = "<span font=\"Terminus 9 Bold\">" ++ str ++
-                                 "</span>"
-                         str'' = sanitize str'
-                     msg <- newSignal "/org/xmonad/Log" "org.xmonad.Log"
-                                "Update"
-                     addArgs msg [String str'']
-                     -- If the send fails, ignore it.
-                     send dbus msg 0 `catchDyn`
-                       (\ (DBus.Error _name _msg) ->
-                         return 0)
-                     return ()
-                 , ppTitle    = pangoColor "#FFFFFF"
-                 , ppCurrent  = pangoColor "#009999" . wrap "[" "]"
-                 , ppVisible  = pangoColor "#996699" . wrap "(" ")"
-                 , ppHidden   = wrap " " " "
-                 , ppUrgent   = pangoColor "red"
-                 , ppLayout   = pangoColor "#999999"
-                 },
-        layoutHook = desktopLayoutModifiers $ myLayout,
-        startupHook = setWMName "LG3D" -- Makes Java GUIs work
+  xmonad $ xfceConfig {
+    logHook = dynamicLogWithPP (prettyPrinter dbus),
+    layoutHook = desktopLayoutModifiers $ myLayout,
+    startupHook = setWMName "LG3D" -- Makes Java GUIs work
     } `additionalKeysP` (
     [ ("M-d", kill)
     , ("M-t", windows W.focusDown)
@@ -76,27 +43,48 @@ main = withConnection Session $ \ dbus -> do
     , ("M-`", nextScreen)
     , ("S-M-`", shiftNextScreen)
     , ("M-0", shiftTo Next EmptyWS)
-    --] ++ [
-    --    (otherModMasks ++ "M-" ++ [key], action tag)
-    --    |   (tag, key)  <- zip (XMonad.workspaces defaultConfig) "123456789"
-    --    , (otherModMasks, action) <- [ ("", windows . W.view)
-    --                           , ("S-", windows . W.shift)]
     ])
 
-    --`additionalKeys`
-    --    [ ((0, xK_Print), withFocused $ \w -> spawn ("/home/bruno/bin/screenshot " ++ show w))
-        --, ((modMask .|. xK_Print), spawn ("/home/bruno/bin/screenshot root"))
-    --    ]
+prettyPrinter :: Connection -> PP
+prettyPrinter dbus = defaultPP
+    { ppOutput   = dbusOutput dbus
+    , ppTitle    = pangoSanitize
+    , ppCurrent  = pangoColor "green" . wrap "[" "]" . pangoSanitize
+    , ppVisible  = pangoColor "yellow" . wrap "(" ")" . pangoSanitize
+    , ppHidden   = const ""
+    , ppUrgent   = pangoColor "red"
+    , ppLayout   = const ""
+    , ppSep      = " "
+    }
+
+getWellKnownName :: Connection -> IO ()
+getWellKnownName dbus = tryGetName `catchDyn` (\(DBus.Error _ _) -> getWellKnownName dbus)
+  where
+    tryGetName = do
+        namereq <- newMethodCall serviceDBus pathDBus interfaceDBus "RequestName"
+        addArgs namereq [String "org.xmonad.Log", Word32 5]
+        sendWithReplyAndBlock dbus namereq 0
+        return ()
+
+dbusOutput :: Connection -> String -> IO ()
+dbusOutput dbus str = do
+    msg <- newSignal "/org/xmonad/Log" "org.xmonad.Log" "Update"
+    addArgs msg [String ("<b>" ++ str ++ "</b>")]
+    -- If the send fails, ignore it.
+    send dbus msg 0 `catchDyn` (\(DBus.Error _ _) -> return 0)
+    return ()
 
 pangoColor :: String -> String -> String
 pangoColor fg = wrap left right
- where
-  left  = "<span foreground=\"" ++ fg ++ "\">"
-  right = "</span>"
+  where
+    left  = "<span foreground=\"" ++ fg ++ "\">"
+    right = "</span>"
 
-
-sanitize :: String -> String
-sanitize [] = []
-sanitize (x:rest) | fromEnum x > 127 = "&#" ++ show (fromEnum x) ++ "; " ++
-                                       sanitize rest
-                  | otherwise        = x : sanitize rest
+pangoSanitize :: String -> String
+pangoSanitize = foldr sanitize ""
+  where
+    sanitize '>'  xs = "&gt;" ++ xs
+    sanitize '<'  xs = "&lt;" ++ xs
+    sanitize '\"' xs = "&quot;" ++ xs
+    sanitize '&'  xs = "&amp;" ++ xs
+    sanitize x    xs = x:xs
